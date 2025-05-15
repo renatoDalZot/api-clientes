917b68e62a3b98016731a1f2824234f979b3db6c
Renato Dal Zot <renatodalzot@hotmail.com>
2025-05-15 13:53:35 -0300

    Implementação do @ConditionalOnProperty com a finalidade de permitir duas rotas idênticas a serem ativadas ou desativadas no aplication.properties - NÃO ESTÁ FUNCIONANDO!

--- src/main/java/com/example/api/clientes/controller/EnderecoController.java
+++ src/main/java/com/example/api/clientes/controller/EnderecoController.java
@@ -7,6 +7,8 @@ import com.example.api.clientes.infraestrutura.EnderecoConsulta;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,11 +22,19 @@ public class EnderecoController {
     private final EnderecoService enderecoService;
     private final EnderecoConsulta enderecoConsulta;
 
+    @ConditionalOnProperty (name = "app.endereco.cadastrar.via.json", havingValue = "true", matchIfMissing = true)
     @PostMapping("/v1/clientes/{cpf}/endereco")
-    public ResponseEntity<EnderecoResponse> cadastrar (@PathVariable @NotEmpty String cpf, @RequestBody @Valid EnderecoRequest enderecoRequest) {
+    public ResponseEntity<EnderecoResponse> cadastrarJson (@PathVariable @NotEmpty String cpf, @RequestBody @Valid EnderecoRequest enderecoRequest) {
         return enderecoService.cadastrar(cpf, enderecoRequest)
                         .map(body -> ResponseEntity.created(URI.create("/v1/clientes/" + cpf + "/endereco/" + body.id())).body(body))
                         .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @ConditionalOnProperty (name = "app.endereco.cadastrar.via.plaintext", havingValue = "true", matchIfMissing = false)
+    @PostMapping("/v1/clientes/{cpf}/endereco")
+    public ResponseEntity<EnderecoResponse> cadastrarPlainText (@PathVariable @NotEmpty String cpf, @RequestBody @Valid EnderecoRequest enderecoRequest) {
+        // TODO: implementar
+        return ResponseEntity.badRequest().build();
     }
 
     @GetMapping("/v1/clientes/{cpf}/endereco")
